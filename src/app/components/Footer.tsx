052e98dbb72e4b89ad63c785e9bef5c0eafd0193
HananGK <hanangabarron@gmail.com>
2024-08-16 19:55:25 +0200

    arreglando footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer () {
     return (
-        <footer className="py-4 my-4 flex items-center justify-center gap-2 border border-gray-300 rounded-full w-[425px] mx-auto">
+        <footer className="py-4 my-4 flex items-center justify-center gap-2 border border-gray-300 rounded-full w-[375px] mx-auto">
             Made with <Image src="/corazon.png" alt="heart" className="inline" width={25} height={25}/> by <a href="https://linkedin.com/in/hanangabarron" target="_blank" rel="noopener noreferrer" className="hover:font-semibold hover:underline">Hanan Gabarrón Kalito</a>
         </footer>
     );
